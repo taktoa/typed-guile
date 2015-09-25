6bb4e647ef0087543528f685d267eb5ddf2befc0
Remy Goldschmidt <taktoa@gmail.com>
2015-09-25 12:09:33 -0500

    Minor cleanup in Variants.hs

--- impl/haskell/library/TypedGuile/Variants.hs
+++ impl/haskell/library/TypedGuile/Variants.hs
@@ -11,19 +11,19 @@
 {-# LANGUAGE UnicodeSyntax         #-}
 
 -- | Extensible variants for small-pass compilation.
--- | Based on the work in
--- | "Variations on Variants", J. Garret Morris
--- | <http://homepages.inf.ed.ac.uk/jmorri14/pubs/morris-haskell15-variants.pdf>
+--   Based on the work in
+--   "Variations on Variants", J. Garret Morris
+--   <http://homepages.inf.ed.ac.uk/jmorri14/pubs/morris-haskell15-variants.pdf>
 module Variants where
 
 data Y
 data N
 
-data Fix e = In (e (Fix e))
+data Fix ε = In (ε (Fix ε))
 
-data (f ⊕ g) e = InL (f e) | InR (g e)
+data (f ⊕ g) ε = InL (f ε) | InR (g ε)
 
-(▽)::(f e -> a) -> (g e -> a) -> (f ⊕ g) e -> a
+(▽) :: (φ1 ε  → α) → (φ2 ε → α) → (φ1 ⊕ φ2) ε → α
 (f ▽ _) (InL x) = f x
 (_ ▽ g) (InR x) = g x
 
@@ -53,7 +53,7 @@ type family Ifi lP inR rP inL where
   Ifi lP inR rP inL = N
 
 class Inj f g p where
-  injP :: p -> f e -> g e
+  injP :: p → f e → g e
 
 instance Inj f f Refl where
   injP _ = id
@@ -64,13 +64,13 @@ instance Inj f g p => Inj f (g ⊕ h) (L p) where
 instance Inj f h p => Inj f (g ⊕ h) (R p) where
   injP (_ :: R p) = InR . injP (undefined :: p)
 
-inj :: forall f g e . (Inj f g (Into f g)) => f e -> g e
+inj :: forall f g e . (Inj f g (Into f g)) => f e → g e
 inj = injP (undefined :: Into f g)
 
-data OnL (h :: * -> *)
-data OnR (h :: * -> *)
-data Le  (g :: * -> *) p
-data Ri  (f :: * -> *) p
+data OnL (h :: * → *)
+data OnR (h :: * → *)
+data Le  (g :: * → *) p
+data Ri  (f :: * → *) p
 data Found
 
 type family Minus f g where
@@ -91,6 +91,8 @@ type family OutOf p where
   OutOf (Le f p) = OutOf p ⊕ f
   OutOf (Ri f p) = f ⊕ OutOf p
 
+type ε1 ⊖ ε2 = Minus ε1 ε2
+
 data CL ε -- lift
 data Cλ ε -- lambda
 data Cx ε -- product
@@ -99,14 +101,16 @@ data CN ε -- noop
 
 type α × β = (α, β)
 
-type FExpr α ε = Expr α (Fix ε)
 
+-- Conveniences for writing coproducts of multiple types
 type Σ1 ε1          κ = ε1                ⊕ κ
 type Σ2 ε1 ε2       κ = ε1 ⊕ ε2           ⊕ κ
 type Σ3 ε1 ε2 ε3    κ = ε1 ⊕ ε2 ⊕ ε3      ⊕ κ
 type Σ4 ε1 ε2 ε3 ε4 κ = ε1 ⊕ ε2 ⊕ ε3 ⊕ ε4 ⊕ κ
 
-type ε1 ⊖ ε2 = Minus ε1 ε2
+
+-- Convenience for fixed-point
+type FExpr α ε = Expr α (Fix ε)
 
 data Expr α e where
   ELift ::  α                                     → FExpr α        CL
@@ -115,7 +119,7 @@ data Expr α e where
   EApp  ::  FExpr (α → β) ε1         → FExpr α ε2 → FExpr β       (Σ2 ε1 ε2 Cβ)
   ENoop ::  FExpr α ε                             → FExpr α       (ε ⊕ CN)
 
-removeNoop :: FExpr α ε -> Expr α (ε ⊖ CN)
+removeNoop :: FExpr α ε → Expr α (ε ⊖ CN)
 removeNoop = undefined
 
 main :: IO ()
